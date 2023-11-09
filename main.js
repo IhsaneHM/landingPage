5f7fcf40d397ac040c372010bbd327c78068653d
hmoumid <ihssanehmoumid42@gmail.com>
2023-11-09 19:55:01 +0100

    add project

---
+++ main.js
@@ -0,0 +1,10 @@
+let img =  document.querySelector('.img');
+let container =  document.querySelector('.container');
+
+
+function donuts(donut){
+    img.src=donut ;
+}
+function bgs(bg){
+    container.style.background=bg;
+}
